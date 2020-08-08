d522504a02cee1c49992d0b72ec40597b60cfb6b
Shivansh-007 <69356296+Shivansh-007@users.noreply.github.com>
2020-08-08 09:51:10 +0530

    Update game.py

--- Guess_The_Number/game.py
+++ Guess_The_Number/game.py
@@ -1,19 +1,49 @@
 from random import randint
+import sys
 
-num_range = 0, 20
-secret_num = randint(*num_range)
+answer = randint(int(sys.argv[1]), int(sys.argv[2]))
+guess_try = 0
+
+print('''
+AHOY! I\'m the Dread Pirate Roberts, and I have a secret!
+Hint: It is a number between {}~{}. Best of luck matey!
+I give you 8 tries 
+'''.format(sys.argv[1], sys.argv[2]))
+
+print('The answer:')
+print(answer)
 
 while True:
     try:
-        user_num = int(
-            input(f'Guess a number between {num_range[0]}-{num_range[1]}: '))
-        if user_num > secret_num:
-            print('Too high...')
-        elif user_num < secret_num:
-            print('Too low...')
+        guess = int(input('guess a number {}~{}:  '.format(sys.argv[1], sys.argv[2])))
+        plus_minus = guess // 10
+
+        if guess_try < 8:
+            if int(sys.argv[1]) < guess < int(sys.argv[2])+1:
+                guess_try += 1
+
+                if guess == answer:
+                    print('Avast! Ye got it! Found my secret, ye did!')
+                    break
+                elif (guess < (answer + plus_minus)) and (guess > answer):
+                    print('High, landlubber!')
+                elif guess > (answer + plus_minus-1):
+                    print('Too high, landlubber!')
+                elif (guess > (answer - plus_minus)) and (guess < answer):
+                    print('Low, ye scruwy dog!')
+                elif guess < (answer - plus_minus+1):
+                    print('Too low, ye scurvy dog!')
+
+            else:
+                print('hey bozo, I said {}~{}'.format(sys.argv[1], sys.argv[2]))
+
         else:
-            print('Hit!')
-            break
+            print('No more guesses! Better luck next time, matey!')
+            print('The secret number was {}'.format(answer))
+            quit()
     except ValueError:
-        print('Please input a valid number!')
-    print()
+        print('please enter a number')
+        continue
+
+print('Thankyou...')
+print('Bye...')
